0342219ea78635fb612f29a160f580e440f006b8
Sujay Nair <sujaynair03@gmail.com>
2018-09-22 14:38:08 -0700

    some games

--- Bowling_ram.py
+++ Bowling_ram.py
@@ -4,11 +4,11 @@ import time
 
 env = gym.make('Bowling-ram-v0')
 env.reset()
-actions = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+actions = [1,2,3,4,5,1,2,3,4,5,1,2,3,4,5]
 print(env.unwrapped.get_action_meanings())
 for i in range(1000000):
-    # action = 1
-    ob, reward, over, _ = env.step(actions[(i % len(actions))])
-    print(reward)
-    env.render()
+	# action = 1
+	ob, reward, over, _ = env.step(actions[(i % len(actions))])
+	print(reward)
+	env.render()
 print "score = 33"
--- pong.py
+++ pong.py
@@ -5,14 +5,29 @@ from utils import getPongState
 
 
 env = gym.make('Pong-v0')
-env.reset()
-actions = [2, 3]
 print(env.unwrapped.get_action_meanings())
-for i in range(1000000):
-    action = 1
-    ob, reward, over, _ = env.step(actions[(i % len(actions))])
-    print(getPongState(ob))
-
-    env.render()
-    time.sleep(0.1)
-print "score = 1750"
+for ep in range(1000):
+	env.reset()
+	next_action = 0
+	for step in range(1000000):
+	    action = 1
+	    ob, reward, over, _ = env.step(next_action)
+	    my_y, opp_y, ball_x, ball_y = getPongState(ob)
+	    if my_y > ball_y:
+	    	next_action = 2
+	    elif my_y < ball_y:
+	    	next_action = 3
+	    elif my_y == ball_y:
+	    	next_action = 2
+	    print my_y, ball_y, next_action
+	    if step % 2 == 0:
+	    	next_action = 0
+	    # if (np.abs(ball_y - my_y) <= 10):
+	    # 	next_action = 0
+	    env.render()
+	    # print over
+	    # time.sleep(0.1)
+	    if over:
+	    	break
+    # time.sleep()
+print "score = "
